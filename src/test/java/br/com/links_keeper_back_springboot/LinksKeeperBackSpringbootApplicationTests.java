b84d32dc259328495a37b21aa864c2454f1bad68
leandroltfx <leandroltfbr@hotmail.com>
2026-08-20 08:32:36 -0300

    Inicia o projeto

---
+++ src/test/java/br/com/links_keeper_back_springboot/LinksKeeperBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.links_keeper_back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksKeeperBackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
