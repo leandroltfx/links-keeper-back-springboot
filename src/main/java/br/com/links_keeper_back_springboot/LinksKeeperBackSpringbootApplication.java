4d08449e134dfd13b685be0da0dbe2753276c81c
leandroltfx <leandroltfbr@hotmail.com>
2026-09-10 08:16:40 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/links_keeper_back_springboot/LinksKeeperBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.links_keeper_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksKeeperBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksKeeperBackSpringbootApplication.class, args);
+	}
+
+}
